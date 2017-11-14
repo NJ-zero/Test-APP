3e223809fd48471ebc64df0fb13632837f466b45
NJ-zero <821854810@qq.com>
2017-11-14 17:17:36 +0800

    运行所有测试用例

---
+++ Testset/runtest.py
@@ -0,0 +1,33 @@
+#coding=utf-8
+#author='Shichao-Dong'
+
+import sys,os
+reload(sys)
+sys.path.append(r'E:\TestAPP\Testset\testcase')
+import unittest
+import HTMLTestRunner
+import time
+from cm.test_cm_list import store_list
+from visit.test_visit_temp import visit_temp
+
+
+def testsuit():
+    suite = unittest.TestSuite()
+    suite.addTests([unittest.defaultTestLoader.loadTestsFromTestCase(store_list),
+                   unittest.defaultTestLoader.loadTestsFromTestCase(visit_temp)
+
+
+
+                   ])
+
+    now=time.strftime("%y-%m-%d-%H-%M-%S")
+    dir = r'E:\\TestAPP\\Result\\report\\'
+    filename=dir +now+'result.html'
+    fp=open(filename,'wb')
+    runner=HTMLTestRunner.HTMLTestRunner(stream=fp,title='test result',description=u'result:')
+
+    runner.run(suite)
+    fp.close()
+
+if __name__ =="__main__":
+    testsuit()
